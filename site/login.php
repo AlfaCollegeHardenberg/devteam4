ccab837cdab3ffdb11e1977019e17a446872af9c
Jonellvl <Jonell98@hotmail.com>
2016-01-19 10:40:13 +0100

    moi

---
+++ site/login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login pagina</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+    <header>
+       <center>
+        <img class="logo" src="logo.jpg" height="10%" width="30%">
+        </center>
+    </header>
+    
+    <div id="login">
+        <h1>Login mijn actie.</h1>
+        <hr>
+        <p>Welkom. volgt u een opleiding bij het alfa-college of werkt u bij het afla-college? Log hier in op de digitale voorzieningen van het alfa-college.</p>
+        <hr>
+        <p>
+        Inlognaam: <input class="inlognaam" type="text" name="username"><br>
+        Wachtwoord: <input class="wachtwoord" type="password" name="username"><br>
+        </p>
+        
+        <button class="login-button">
+            Login
+        </button>
+        
+        <hr>
+        
+        <li><p>Uw wachtwoord (en/of gebruikersnaam) vergeten? Ga dan naar het servicepunt/receptie op uw locatie.</p></li>
+        <li><p>Voor het wijzigen van uw wachtwoord dient u eerst in te loggen.</p></li>
+        <li><p>klik <a href="https://registratie.alfa-college.nl/secure/Conditions.aspx">hier</a> voor de gebruikersvoorwaarden.</p></li>
+    </div>
+    
+    <footer>
+        <img class="footer" src="footer.png">
+    </footer>
+</body>
+</html>
