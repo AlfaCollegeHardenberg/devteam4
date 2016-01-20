6e89fb7bea39130839052f827a10023d441c0f24
jelmerbou <jelmer_bouman@hotmail.com>
2016-01-20 13:16:30 +0100

    anfkanf

--- login_pagina/login.php
+++ login_pagina/login.php
@@ -1,4 +1,39 @@
+<!-- verbinding maken met db-connect en kijken of er iemand is ingelogd-->
+<?php 
+    require_once "connect.php";
 
+if (($_SESSION['loggedin'] == true)){
+		echo "<a href='log-out.php'> Uitloggen </a> ";
+} else {
+    header("Location: login.php");
+}
+?>
+
+<!-- Als er niet is ingelogd komt die hier uit -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login pagina</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+      
+<!-- Header met het logo van de pagina -->
+   
+    <header>
+       <center>
+        <img class="logo" src="Afbeeldingen/logo.jpg" height="10%" width="30%">
+        </center>
+    </header>
+                
+<!-- Div met alle text en login velden -->
+    
+    <div id="login">
+        <h1>Log in mijn actie.</h1>
+        <hr>
+        <p>Welkom. volgt u een opleiding bij het alfa-college of werkt u bij het afla-college? Log hier in op de digitale voorzieningen van het alfa-college.</p>
         <hr>
 
 <!-- login verlden -->
