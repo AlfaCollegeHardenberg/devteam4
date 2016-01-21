c375b0c327e7918b535478fea62a24c5b32f0c58
Jonellvl <Jonell98@hotmail.com>
2016-01-21 10:28:56 +0100

    Materialen pagina

---
+++ materialen_pagina/index.php
@@ -0,0 +1,105 @@
+<?php
+	require_once 'db-connect.php';
+	
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Actie Alfa College</title>
+<link rel="stylesheet" href="style.css">
+<link href='https://fonts.googleapis.com/css?family=Roboto:400,300italic' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+<header>
+            <img class="rareVormenBoven" src="Afbeeldingen/raareVormenBoven.png">
+    <div id="logo_header">
+        <img src="Afbeeldingen/actieLogo.png">
+    </div>
+    <div id="nav_header">
+        <ul>
+            <li><a class="home" href="#">Home</a></li>
+            <li><a class="wegwijzer" href="#">Wegwijzer</a>
+			
+			   <ul class="hidden">
+				<li><a href="#">Who We Are</a></li>
+				<li><a href="#">What We Do</a></li>
+				
+			</ul>
+			</li>
+            <li><a class="mijnSchool" href="#">Mijn school</a></li>
+            <li><a class="programmas" href="#">Programma's</a></li>
+        </ul>
+    </div>
+</header>
+<div class="inhoud">
+		<table class="demo">
+	<caption>Lesmaterialen</caption>
+	<thead>
+	<tr>
+		<th>Type</th>
+		<th>Naam</th>
+		<th>Gewijzigd</th>
+		<th>Gewijzigd door</th>
+	</tr>
+	</thead>
+	<tbody>
+	<tr>
+		<td>Folder</td> <!-- Type //-->
+		<td><a href="">Drupal</a></td> <!-- Naam //-->
+		<td>2-9-2015</td> <!-- Gewijzigd //-->
+		<td>Sieljes, Rob</td> <!-- Gewijzigd door //-->
+	</tr>
+	<tr>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+	</tr>
+	<tr>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+	</tr>
+	<tr>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+	</tr>
+	<tr>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+	</tr>
+	<tr>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+	</tr>
+	<tr>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+	</tr>
+	<tr>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+	</tr>
+	</tbody>
+</table>
+	</div>
+</div>
+<footer>
+</footer>
+</body>
+</html>
